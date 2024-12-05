9c8b555621e14985bcaf03a6ee58e1bd03b211ec
Aziz Kamoun <aziz.kamoun@gmail.com>
2024-12-04 20:38:05 -0800

    Update create_paf_script_demo.bash

--- create_paf_script_demo.bash
+++ create_paf_script_demo.bash
@@ -1,4 +1,4 @@
-inimap2 -x asm5 Orthoebolavirus_bombaliense_cds_from_genomic.fna Orthoebolavirus_restonense_cds_from_genomic.fna > bombaliense_restonese_cds_comparison.paf
+minimap2 -x asm5 Orthoebolavirus_bombaliense_cds_from_genomic.fna Orthoebolavirus_restonense_cds_from_genomic.fna > bombaliense_restonese_cds_comparison.paf
 
 jbrowse add-track bombaliense_restonese_cds_comparison.paf --assemblyNames=Orthoebolavirus_restonense_cds_from_genomic.fna,Orthoebolavirus_bombaliense_cds_from_genomic.fna --load copy --out $APACHE_ROOT/jbrowse2
 
