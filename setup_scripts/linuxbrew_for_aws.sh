5215edf8c86ee65f91fbb528ec49a76a4ade4ae5
Aziz Kamoun <aziz@pared.com>
2024-12-07 15:10:46 -0800

    [Aziz] Break out second script

--- setup_scripts/linuxbrew_for_aws.sh
+++ setup_scripts/linuxbrew_for_aws.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-# sudo su -
 
-# passwd ubuntu
-
-# exit
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo >> /home/ubuntu/.bashrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"f
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
