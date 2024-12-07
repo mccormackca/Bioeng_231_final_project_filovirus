08fe0279fdc06a3e8832cc20a665655ed4cd4cfe
Aziz Kamoun <aziz@pared.com>
2024-12-07 15:53:20 -0800

    [Aziz] setup scripts to merge, for now

--- setup_scripts/start_apache_server.sh
+++ setup_scripts/start_apache_server.sh
@@ -7,8 +7,6 @@ mkdir ~/tmp
 
 cd ~/tmp
 
-export APACHE_ROOT='/var/www/html'
-
 jbrowse create output_folder
 sudo mv output_folder $APACHE_ROOT/jbrowse2
 sudo chown -R $(whoami) $APACHE_ROOT/jbrowse2
