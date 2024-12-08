cc15eae363b6455fde548f803ae7381bdc48184d
mccormackca <cmccormack@berkeley.edu>
2024-12-07 22:09:52 -0800

    Create one script for generating all MSA and tree files generate_MSAs_and_trees.sh

--- selected_preloaded_data/scripts/generate_MSAs_and_trees.sh
+++ selected_preloaded_data/scripts/generate_MSAs_and_trees.sh
@@ -1,6 +1,16 @@
 cd ~/tmp
 # install seqtk for parsing FASTAs
 sudo apt-get -y install seqtk
+# install clustal omega for generating multiple sequence alignment files
+sudo apt -y install clustalo
+# install iqtree for generating phylogenetic trees
+sudo apt-get install iqtree
+##has a y/n prompt about memory usage. Choose y to intall
+
+# combine full genome sequences into one fasta file
+cat Orthoebolavirus_bombaliense.fna Orthoebolavirus_restonense.fna Orthoebolavirus_sudanense.fna Orthoebolavirus_taiense.fna Orthoebolavirus_zairense.fna >> all_genomes.fna
+
+##make fasta files with the sequences of one protein across all five genomes
 
 # add protein seqs from Orthoebolavirus zairense to respective files
 # make a .txt file with the sequence id for the NP gene (nucleoprotein)
@@ -46,3 +56,20 @@ echo "YP_009513281.1" > "id_vp24.txt"
 seqtk subseq Orthoebolavirus_bombaliense_protein.faa id_vp24.txt >> vp24.fasta
 echo "YP_009513275.1" > "id_vp35.txt"
 seqtk subseq Orthoebolavirus_bombaliense_protein.faa id_vp35.txt >> vp35.fasta
+
+#Generate multiple sequence alignments
+
+# run clustal omega on full genomes
+clustalo -i all_genomes.fna -o all_genomes_aligned.fasta
+# run clustal omega on nucleoprotein sequences
+clustalo -i all_genomes.fna -o NP.fasta
+# run clustal omega on membrane-associated protein (VP24) sequences
+clustalo -i all_genomes.fna -o vp24.fasta
+# run clustal omega on polymerase complex protein (VP35) sequences
+clustalo -i all_genomes.fna -o vp35.fasta
+
+#Generate phylogenetic tree files using IQtree2
+iqtree2 -s all_genomes_aligned.fasta
+iqtree2 -s NP_aligned.fasta
+iqtree2 -s vp24_aligned.fasta 
+iqtree2 -s vp35_aligned.fasta
